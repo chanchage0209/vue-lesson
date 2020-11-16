f830224c45a85a68303d509f7cb70102d4ed2ba6
unknown <chanchage0209@gmail.com>
2020-11-16 09:12:08 +0800

    VT 11/16

---
+++ src/APPwatch.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <TodoList/> -->
+  <div>
+    <h2>{{ countDouble }}</h2>
+    <button @click="countDouble += 1"></button>
+  </div>
+</template>
+
+<script>
+// import TodoList from "./TodoList";
+
+
+export default {
+  // components: { TodoList },
+  data:()=>({
+    count_:0,
+  }),
+  computed:{
+        
+  },
+  watch:{
+    if(count_%10===0){
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
